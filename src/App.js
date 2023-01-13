bea74eb64a74299e79c4ed85c52b92960c763a3b
brandmch <101467867+brandmch@users.noreply.github.com>
2023-01-13 12:49:12 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { marked } from "marked";
+
+const renderText = (text) => {
+  const __html = marked(text, { sanitize: true });
+  return { __html };
+};
+
+const Editor = ({ setState }) => {
+  return (
+    <div className="editor">
+      <textarea
+        rows={15}
+        cols={60}
+        onChange={(event) => setState(event.target.value)}
+      ></textarea>
+    </div>
+  );
+};
+
+const Previewer = ({ state }) => {
+  return (
+    <div className="previewer">
+      <div dangerouslySetInnerHTML={renderText(state)} />
+    </div>
+  );
+};
 
 function App() {
+  const [text, setText] = useState("");
+  console.log(text);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor setState={setText} />
+      <Previewer state={text} />
+      <footer>Brandon McHugh 2022</footer>
     </div>
   );
 }
